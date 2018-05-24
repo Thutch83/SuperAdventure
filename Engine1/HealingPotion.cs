8f380a8bc2a1f6ce826ae9cd127c09d51026f195
Tim Hutchinson <Tim.Hutchinson83@gmail.com>
2018-05-23 22:18:52 -0500

    First entry, posting what I've been working on offline.

---
+++ Engine1/HealingPotion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class HealingPotion : Item
+    {
+               public int AmountToHeal { get; set; }
+    }
+}
